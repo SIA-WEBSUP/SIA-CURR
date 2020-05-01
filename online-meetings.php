c414e7242abd289405c83c04f0599fd50859740b
davidve-nyc <44128751+dveintimilla@users.noreply.github.com>
2020-05-01 00:17:35 -0400

    Removed Open Books from online meetings table

--- online-meetings.php
+++ online-meetings.php
@@ -1,104 +1,11 @@
 <?php
 
-
-include('./process/sql-connect.php');
-
-// use david v's credentials
-$dbuser		= isset($_GET['jdv']) ? 'suffolk_testing': $dbuser;
-$dbpass 	= isset($_GET['jdv']) ? 'WEstaging2aa47' : $dbpass;
-
-//make sure errors are being reported
-error_reporting(E_ALL);
-
-//connect to database
-try {
-	$pdo = new PDO('mysql:charset=UTF8;dbname=' . $dbname . ';host=' . $dbhost, $dbuser, $dbpass);
-} catch (PDOException $e) {
-	die('Database connection failed: ' . $e->getMessage());
-}
-
-//error handling
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-//select data
-try {
-	$result = $pdo->query('SELECT * FROM `new_meeting`');
-} catch (PDOException $e) {
-	die('SQL query failed: ' . $e->getMessage());
-}
-
-function dd($obj) {
-	echo '<pre>';
-	print_r($obj);
-	exit;
-}
-
-function array_flatten($array, $return=array()) {
-	for ($x = 0; $x <= count($array); $x++) {
-		if (isset($array[$x])) {
-			if (is_array($array[$x])) {
-				$return = array_flatten($array[$x], $return);
-			} else {
-				$return[] = $array[$x];
-			}
-		}
-	}
-	return $return;
-}
-
-$type_lookup = array(
-	'SA' => 'A',
-	'ASL' => 'ASL', // ​American Sign Language
-	'ABSI' => 'ABSI', // As Bill Sees It
-	'ABSIT' => 'ABSI', // As Bill Sees It
-	'B' => 'BE', // Newcomer
-	'BB' => 'B', // Big Book
-	'BBOD' => array('B', 'O', 'D'), // Big Book, Open, Discussion
-	'BYOC' => 'C', // Bring Your Own Closed
-	'C' => 'C', // Closed
-	'CD' => array('C', 'D'), // Closed, Discussion
-	'DR' => 'DR', // Daily Reflections
-	'GV' => 'GR', // Grapevine
-	'GLBT' => 'LGBTQ', // GLBT
-	'LIT' => 'LIT', // Literature
-	'LS' => 'LS', // Living Sober
-	'M' => 'M', // Mens
-	'MC' => array('M', 'C'), // Mens, Closed
-	'MED' => array('MED','11'), // Meditation
-//	'MEDITATION' => 'MED', // Meditation
-	'O' => 'O', // Open
-	'OB' => array('O', 'BE'), // Open, Newcomer
-	'OBB' => array('O', 'B'), // Open, Big Book
-	'OD' => array('O', 'D'), // Open, Discussion
-	'ODB' => array('O', 'D', 'BE'), // Open, Discussion, Newcomer
-    'ONL' => 'ONL', // Temp Online Meeting
-	'OS' => array('O', 'ST'), // Open, Step Meeting
-	//[24] => PROMISES = Promises
-	'S' => 'ST', // Step
-	'SP' => 'S', // Spanish
-	'T' => 'TR', // Tradition
-    'TC' => 'TC', // Temp Closure
-	'TOPIC' => 'D', // Topic
-	'W' => 'W', // Womens
-	'WC' => array('W', 'C'), // Womens, Closed
-	'WCD' => array('W', 'C', 'D'), // Womens, Closed, Discussion
-	'WS' => array('W', 'ST'), // Womens, Step Meeting
-	'YP' => 'Y', // Young People
-	'X' => 'X', // Handicap
-);
-
-$type_keys = array_keys($type_lookup);
-
-$day_lookup = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-
-$all_types = array();
-
 $online_mtgs = array( // Most groups have one URL for all meetings, some have both group level URL and meeting URLs
     // 'GROUPID'  => array("conf_URL",	NULL,"mtg_id", "pwd"), // GROUP NAME
     '37'  => array("//zoom.us/j/6052451111?pwd=c085c0o15V1RWWWlBNkFYTmNVUE93UT09",	"(929) 205-6099","605 245 1111", "428 950"), // BAY SHORE SUNRISE SOBRIETY
     '67'  => array("//zoom.us/j/691630673         ","(646) 558-8656","691 630 673","850 366"), // SUNRISE SOBRIETY - 6:45AM
     '82'  => array("//zoom.us/j/6311431023        ","(631) 766-3661","631 143 1023", "Cutchogue"),     // CUTCHOGUE SUNDAY
-  //'85'  => array("//us04web.zoom.us/j/742133476", NULL, NULL, NULL),                              // HOME FOR DINNER
+    //'85'  => array("//us04web.zoom.us/j/742133476", NULL, NULL, NULL),                              // HOME FOR DINNER
     '104' => array("//nyintergroup.zoom.us/j/96853910721?pwd=L1R5bmFDdVoyUFM2Szh5dUZXZU10UT09","(929) 436-2866", "968 5391 0721", "521231"), // EYE OPENER
     '104.SU.0' => array("//nyintergroup.zoom.us/j/98293111250?pwd=N3orUFNBNFpSVjZiV1JTM0Y5TVNZZz09","(929) 436-2866", "982 9311 1250", "521231"), // EYE OPENER - MENS
     '104.SU.1' => array("//zoom.us/j/93088948413","(929) 436-2866", "930 8894 8413", "788615"), // EYE OPENER - WOMENS
@@ -141,7 +48,7 @@ $online_mtgs = array( // Most groups have one URL for all meetings, some have bo
     '307.TH.0' => array("//meet.google.com/sps-xjqn-uiu","(614) 602-6125","848 749 635", NULL), // STONY BROOK UNITY GRP
     '422.FR.0' => array("//meet.google.com/wno-mhit-zmk","(402) 824-0258","705 336 354", NULL), // ROCKY POINT UNITY GRP
     '422.SA.0' => array("//meet.google.com/wno-mhit-zmk","(402) 824-0258","705 336 354", NULL), // ROCKY POINT UNITY GRP
-    //'584.TH.0' => array("//meet.google.com/mpt-bxbd-syv","(573) 667-3257","961 358 515", NULL), // OPEN BOOKS
+    '584.TH.0' => array("//meet.google.com/mpt-bxbd-syv","(573) 667-3257","961 358 515", NULL), // OPEN BOOKS
     //'049.SU.0' => array("//zoom.us/j/604823002", "(646) 558-8656","604 823 002", NULL), // LADIES GROUP - Sun CD
     //'049.WD.0' => array("//zoom.us/j/667893124", "(646) 558-8656","667 893 124", NULL), // LADIES GROUP - Wed Step
 
@@ -180,136 +87,76 @@ $online_mtgs = array( // Most groups have one URL for all meetings, some have bo
     '439'  => array("//zoom.us/j/697601579",            NULL,"697 601 579", NULL),      // Water Mill WATER MILL SUNDAY NIGHT BEGINNER'S
 );
 
-//fetch data
-$return = array();
-foreach ($result as $row) {
-	//types
-	$row['types'] = str_replace(array(',', ';', ':'), ' ', strtoupper($row['types']));
-	$types = explode(' ', $row['types']);
 
-    // replace <br> with '/n/r'
-    $row['notes'] = str_replace( '<br>', "\n\r", $row['notes']);
-    $row['locationNotes'] = str_replace( '<br>', "\n\r", $row['locationNotes']);
 
-    //online meetings
-    $conference_url = $conference_phone = $conference_mtgID = $conference_pwd = NULL;
+function online_meeting_info($group_id, $meeting_id)
+{
+    $notes = NULL;
+    $URL = NULL;
+    $phone=NULL;
+
+
+    $conference_url = $conference_phone = $conference_mtgID = $conference_pwd = $conference_notes = #access_num = NULL;
     $conference_info = NULL;
     $access_num = NULL;
+    $conference_notes = NULL;
 
-    if ( in_array("TC",$types)) {
-        // meeting_id entry takes precedence over group_id;
-        if (array_key_exists($row['meeting_id'],$online_mtgs)) {
-            $conference_info = $online_mtgs[$row['meeting_id']];
-        } else if (array_key_exists($row['group_id'],$online_mtgs)) {
-            $conference_info = $online_mtgs[$row['group_id']];
+    // meeting_id entry takes precedence over group_id;
+    if (array_key_exists($meeting_id,$online_mtgs)) {
+        $conference_info = $online_mtgs[$meeting_id];
+    } else if (array_key_exists($group_id,$online_mtgs)) {
+        $conference_info = $online_mtgs[$group_id];
+    }
+    if ($conference_info) {
+        $conference_url = $conference_info[0] ? "https:" . $conference_info[0]  : NULL;
+        $conference_phone = $conference_info[1];
+        $conference_mtgID = $conference_info[2];
+        $conference_pwd = $conference_info[3];
+
+        // append mtgID to meeting notes
+        if ($conference_mtgID && strpos($conference_url, "zoom.")) {
+            // Display the ZOOM meeting ID for ALL ZOOM meetings to handle
+            // the rare case where the group doesn't want a direct link to meeting
+            // TODO: remove this now that meeting finder supports connect via zoom button
+            $online_meeting_info['notes'] .= "ZOOM Meeting ID: " . $conference_mtgID . "\n\r\n\r";
+            if ($conference_pwd) {
+                if (strpos(strtoupper($conference_pwd), "PASSWORD"))
+                    // Special Instructions for password
+                    $row['notes'] .= $conference_pwd . "\n\r\n\r";
+                else
+                    $row['notes'] .= "PWD: " . $conference_pwd . "\n\r\n\r";
+
+            }
         }
-        if ($conference_info) {
-            $types[]='ONL';
-            $conference_url = $conference_info[0] ? "https:" . $conference_info[0]  : NULL;
-            $conference_phone = $conference_info[1];
-            $conference_mtgID = $conference_info[2];
-            $conference_pwd = $conference_info[3];
-
-            if (strlen($row['notes'])>0) $row['notes'] .= "\n\r"; // Assume we'll be appending something
-
-            // append mtgID to meeting notes
-            if ($conference_mtgID && strpos($conference_url, "zoom.")) {
-                // Display the ZOOM meeting ID for ALL ZOOM meetings to handle
-                // the rare case where the group doesn't want a direct link to meeting
-                $row['notes'] .= "ZOOM Meeting ID: " . $conference_mtgID . "\n\r\n\r";
-                if ($conference_pwd) {
-                    if (strpos($conference_pwd, "Password"))
-                        // Special Instructions for password
-                        $row['notes'] .= $conference_pwd . "\n\r\n\r";
-                    else
-                        $row['notes'] .= "PWD: " . $conference_pwd . "\n\r\n\r";
-                }
-            }
-            // append phone number to meeting notes
-            if ($conference_phone) {
-                $access_num = strpos($conference_phone, "Access #:");
-                if ($access_num) {
-                    // grab access pin if it exists
-                    $t = substr($conference_phone, 0, $access_num-1);
-                    $access_num = substr($conference_phone, $access_num+strlen("Access #:"), strlen($conference_phone)-1);
-                    $conference_phone = $t;
-                }
-                $row['notes'] .= "To join by phone dial:\n\r  ". $conference_phone;
-                if ($conference_mtgID)
-                    $row['notes'] .= " PIN: " . $conference_mtgID .  "#";
-                $row['notes'] .= "\n\r\n\r";
-            }
-            // append pwd to meeting notes
+        // append phone number to meeting notes
+        if ($conference_phone) {
+            $access_num = strpos($conference_phone, "Access #:");
             if ($access_num) {
-                $row['notes'] .= " ACCESS#: " . $access_num .  "#";
+                // grab access pin if it exists
+                $t = substr($conference_phone, 0, $access_num-1);
+                $access_num = substr($conference_phone, $access_num+strlen("Access #:"), strlen($conference_phone)-1);
+                $conference_phone = $t;
             }
+            $row['notes'] .= "To join by phone dial:\n\r  ". $conference_phone;
+            if ($conference_mtgID)
+                $row['notes'] .= " PIN: " . $conference_mtgID .  "#";
+            $row['notes'] .= "\n\r\n\r";
+        }
+        // append pwd to meeting notes
+        if ($access_num) {
+            $row['notes'] .= " ACCESS#: " . $access_num .  "#";
+        }
 
-            // encode phone with meeting ID and password for one tap number
-            // should look like +12125551212,,123456789#,,#,,444444#
-            if ($conference_phone) {
-                $conference_phone = "+1" . $conference_phone . ",," . $conference_mtgID . "#";
-                if ($access_num) {
-                    $conference_phone .= ",,#,," . $access_num . "#"; // append access no if it exists
-                } else if ($conference_pwd){
-                    $conference_phone .= ",,#,," . $conference_pwd . "#"; // append pwd if necessary
-                }
-                $conference_phone = str_replace(array('(', ')','-',' '), '', $conference_phone); //strip unnecessary chars
+        // encode phone with meeting ID and password for one tap number
+        // should look like +12125551212,,123456789#,,#,,444444#
+        if ($conference_phone) {
+            $conference_phone = "+1" . $conference_phone . ",," . $conference_mtgID . "#";
+            if ($access_num) {
+                $conference_phone .= ",,#,," . $access_num . "#"; // append access no if it exists
+            } else if ($conference_pwd){
+                $conference_phone .= ",,#,," . $conference_pwd . "#"; // append pwd if necessary
             }
+            $conference_phone = str_replace(array('(', ')','-',' '), '', $conference_phone); //strip unnecessary chars
         }
     }
-
-	//$all_types = array_merge($all_types, $types);
-	$types = array_filter($types, function($type) use ($type_keys) {
-		return in_array($type, $type_keys);
-	});
-	$types = array_map(function($type) use ($type_lookup) {
-		return $type_lookup[$type];
-	}, $types);
-	$types = array_flatten($types);
-
-	//add C whenever meeting is not O
-	//if (!in_array($types, 'O')) $types[] = 'C';
-
-
-	//build array
-	$return[] = array(
-		'slug' => $row['meeting_id'],
-		'day' => array_search($row['day'], $day_lookup),
-		'time' => date('H:i', strtotime($row['time'])),
-		'name' => $row['group_name'],
-		'types' => $types,
-		'location' => $row['locationName'],
-		'address' => $row['locationAddress'],
-		'city' => $row['locationCity'],
-		'state' => $row['locationState'],
-		'postal_code' => $row['locationZip'],
-		'country' => 'US',
-		'region'=> $row['town'],
-		'notes' => $row['notes'],
-		'location_notes' => $row['locationNotes'],
-		'updated' => $row['lastUpdate'],
-		'conference_url' => $conference_url,
-		'conference_phone' => $conference_phone
-	);
 }
-
-/*
-$all_types = array_unique($all_types);
-sort($all_types);
-dd($all_types);
-*/
-
-//encode JSON
-$return = json_encode($return);
-if (json_last_error()) {
-	die('JSON error: ' . json_last_error_msg());
-}
-
-//make sure headers haven't already been sent, will cause error
-if (headers_sent()) {
-	die('Error: headers already sent!');
-}
-
-//output
-header('Content-type: application/json; charset=utf-8');
-echo $return;
