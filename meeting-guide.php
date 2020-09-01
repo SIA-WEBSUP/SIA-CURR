f343eed5ae9fb0a340be524a3f5ecb05ed07aef0
davidve-nyc <44128751+dveintimilla@users.noreply.github.com>
2020-09-01 19:33:45 -0400

    commented out BROOKHAVEN AS BILL SEES IT

--- meeting-guide.php
+++ meeting-guide.php
@@ -122,7 +122,7 @@ $online_mtgs = array( // Most groups have one URL for all meetings, some have bo
     '499' => array("//meet.google.com/dqe-eerp-zgv","(570) 554-0159","614 095 850", NULL), // SPIRITUAL CIRCLE
     '530' => array("//us02web.zoom.us/j/84350888563?pwd=MGZnaGNiYnEvQVRCT2RLZXdtVC8vdz09","(929) 436-2866","843 5088 8563", "297 082"), // STONY BROOK FREETHINKERS
 //    '530' => array("//meet.google.com/bpy-aijb-aqi","(301) 845-5176","836 521 984", NULL), // STONY BROOK FREETHINKERS
-    '553' => array("//zoom.us/j/755341289", NULL, NULL, NULL),                         // AS BILL SEES IT
+//    '553' => array("//zoom.us/j/755341289", NULL, NULL, NULL),                         // AS BILL SEES IT
     '581' => array("//zoom.us/j/504706298", "(929) 205-6099", "504 706 298", NULL),        // FREEDOM FROM FEAR
     '601' => array("//zoom.us/j/98558565837", NULL, "985 5856 5837", "631 740"),           // CONNECT THE DOTS
     '606' => array("//zoom.us/j/86781747581",NULL,"867 8174 7581", "Text for password access: 631-388-0876 or 631-889-1613"), // PORT JEFF TRUE FRIENDS WOMENS
