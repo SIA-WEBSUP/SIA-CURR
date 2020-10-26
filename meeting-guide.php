e7fa4fb4d172969ae46b4b4cb03df66f6f0e0583
David - MSI Game Laptop <dveintimilla@gmail.com>
2020-10-26 19:50:35 -0400

    removed CENTER MORICHES SUNRISE SOBREITY:    Sat 8am Women's    Sun 8am Men's

--- meeting-guide.php
+++ meeting-guide.php
@@ -167,8 +167,6 @@ $online_mtgs = array( // Most groups have one URL for all meetings, some have bo
     '704.SU.1' => array("//nyintergroup.zoom.us/j/94537387870?pwd=TWhvbjcxTzJzM2VhakQ2bEIydmFqZz09","(929) 436-2866", "945 3738 7870", "788615"), // EYE OPENER - WOMENS
     '705'      => array("//zoom.us/j/691630673","(646) 558-8656","691 630 673", "850366"), // SUNRISE SOBRIETY - TUES 7AM
     '705.TU.1' => array("//zoom.us/j/369437303","(646) 558-8656","369 437 303", "039778"), // SUNRISE SOBRIETY - 6:45AM
-    '705.SA.1' => array("//zoom.us/j/678316314","(646) 558-8656","678 316 314", "003597"), // SUNRISE SOBRIETY - WOMENS 8AM
-    '705.SU.1' => array("//zoom.us/j/202548779","(646) 558-8656","202 548 779", "889048"), // SUNRISE SOBRIETY - MENS 8AM
     '707' => array(NULL, "(605) 313-5315","266 443", NULL),                                // ST JAMES SUNRISE REFLECTIONS
     '710' => array("//nyintergroup.zoom.us/j/99111309723?pwd=dXlHcUU1SEIya0pjSEV5NVU3NVJadz09","(929) 436-2866 ","991 1130 9723", "0188"), // A MOMENT OF CLARITY
     '713' => array(NULL ,"(415) 655-0001 Passcode: 132 373 9253", NULL, NULL),             // NORTH BABYLON JUST FOR TODAY
